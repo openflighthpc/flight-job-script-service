aa6b1aa38d1cc034ed0c760c573c1b7b642db696
Ben Armston <ben.armston@alces-software.com>
2021-08-25 17:05:43 +0100

    Remove unused library

--- api/config/post_boot.rb
+++ api/config/post_boot.rb
@@ -35,6 +35,3 @@ require 'logger'
 
 # Shared activesupport libraries
 require 'active_support/core_ext/hash/keys'
-
-# Ensure ApplicationModel::ValidationError is defined in advance
-require 'active_model/validations.rb'
