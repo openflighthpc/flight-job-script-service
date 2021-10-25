79023bd6edf0ce23b86a4c6703e9688df3d7c0ae
Ben Armston <ben.armston@alces-software.com>
2021-10-25 17:45:17 +0100

    circleci integration

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,43 @@
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders without crashing', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Loading.../i);
-  expect(linkElement).toBeInTheDocument();
+async function renderApp() {
+  const utils = render(<App />);
+  expect(utils.getByText('Loading...')).toBeInTheDocument();
+  await waitFor(
+    () => expect(utils.queryByText('Loading...')).toBeNull()
+  );
+  return utils;
+}
+
+test('renders without crashing', async () => {
+  const { queryByText } = await renderApp();
+  expect(queryByText('An error has occurred')).toBeNull();
+});
+
+
+
+test('can sign in', async () => {
+  const {
+    getByText, getByRole, getByLabelText, queryByText,
+  } = await renderApp();
+
+  expect(queryByText(/test-user/)).toBeNull();
+  expect(queryByText(/Test user/)).toBeNull();
+
+  const loginButton = getByRole('button', { name: /Log in/});
+  expect(loginButton).toBeInTheDocument();
+  fireEvent.click(loginButton)
+
+  const nameInput = getByLabelText('Enter your username');
+  const passwordInput = getByLabelText('Enter your password');
+  const button = getByRole('button', { name: 'Sign in' });
+  fireEvent.change(nameInput, { target: { value: 'test-user' } });
+  fireEvent.change(passwordInput, { target: { value: 'test-password' } });
+  fireEvent.click(button);
+  await waitFor(
+    () => expect(queryByText(/Log in/)).toBeNull()
+  )
+  expect(getByText('test-user')).toBeInTheDocument();
+  expect(getByText('Test user')).toBeInTheDocument();
 });
