8cfeb05ab62cc45e1ed65043905fb75997a9efa3
Ben Armston <ben.armston@alces-software.com>
2022-06-27 15:30:05 +0100

    Remove Executor reference

--- client/src/Blurb.js
+++ client/src/Blurb.js
@@ -3,9 +3,9 @@ import React from 'react';
 function Blurb() {
   return (
     <p>
-      The Flight Executor service allows you to create and manage customized
-      job scripts from predefined templates, launch jobs and monitor their
-      activity.
+      The Flight Job Manager service allows you to create and manage
+      customized job scripts from predefined templates, launch jobs and
+      monitor their activity.
     </p>
   );
 }
