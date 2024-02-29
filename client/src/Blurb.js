475401b05408c1c8f670e5e8774a6473b09062f6
megan-godfrey <102584263+megan-godfrey@users.noreply.github.com>
2024-02-29 16:52:05 +0000

    Update blurb

--- client/src/Blurb.js
+++ client/src/Blurb.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import Logo from './assets/job.png';
 
 function Blurb() {
   return (
-    <p>
-      The Flight Job Manager service allows you to create and manage
-      customized job scripts from predefined templates, launch jobs and
-      monitor their activity.
-    </p>
+    <>
+      <div
+        className="app-card blurb"
+      >
+        <img
+          className="app-icon mr-3"
+          alt=""
+          src={Logo}
+        />
+        <h2 className="card-title card-text">
+          flight<strong>Job</strong>
+        </h2>
+        <p className="tagline card-subtitle card-text">
+          Create and manage your jobs.
+        </p>
+      </div>
+    </>
   );
 }
 
